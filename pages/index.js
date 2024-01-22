0df4cf851eafb127b9d3dd91d747aa0665789969
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-01-22 10:54:41 +0100

    boxWithStyledComponents

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import BoxWithStyledComponents from "@/components/BoxWithStyledComponents";
 
 export default function HomePage() {
   return (
     <div>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
+      <BoxWithStyledComponents />
+      <BoxWithStyledComponents $isBlack />
     </div>
   );
 }
